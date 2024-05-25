e55ae4b54008e2249ce436c99b106f60e3b9c8d1
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-25 17:51:31 +0530

    added tailwind

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import LinkButton from '../../ui/LinkButton';
 
 const fakeCart = [
   {
@@ -29,12 +30,12 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <LinkButton to='/menu'>&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
+        <Link to='/order/new'>Order pizzas</Link>
         <button>Clear cart</button>
       </div>
     </div>
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Username() {
+  return <div className='hidden text-sm font-semibold md:block'>Hemanshu</div>;
+}
+
+export default Username;
