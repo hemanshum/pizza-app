53f78514033059273d250a4401795b57b69ff5e5
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-27 00:11:00 +0530

    Redux Toolkit configured

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,6 +2,8 @@ import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
 
+import { useSelector } from 'react-redux';
+
 const fakeCart = [
   {
     pizzaId: 12,
@@ -27,13 +29,15 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector((state) => state.user.username);
+
   const cart = fakeCart;
 
   return (
     <div className=' px-4 py-3'>
       <LinkButton to='/menu'>&larr; Back to menu</LinkButton>
 
-      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
       <ul className='devide-stone-200 mt-3 divide-y border-b'>
         {cart.map((item) => (
           <CartItem item={item} key={item.pizzaId} />
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Username() {
-  return <div className='hidden text-sm font-semibold md:block'>Hemanshu</div>;
+  const username = useSelector((state) => state.user.username);
+  return (
+    <div className='hidden text-sm font-semibold md:block'>{username}</div>
+  );
 }
 
 export default Username;
