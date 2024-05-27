ba089912867e11c1cc79c4bb68ea750c6fc20c3c
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-27 09:39:43 +0530

    card functionality added

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,15 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   // cart: [],
-  cart: [
-    {
-      pizzaId: 12,
-      name: 'Mediterranean',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
 
 const cartSlice = createSlice({
@@ -18,7 +10,7 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addItem: (state, action) => {
-      state.card.push(action.payload);
+      state.cart.push(action.payload);
     },
     deleteItem: (state, action) => {
       state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
@@ -52,3 +44,9 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQty = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
